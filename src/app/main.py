70e6eaf3058ddf0acabce2735c2af5d55142cef2
Yulie Jung <yuliekorea@gmail.com>
2021-12-03 15:46:23 +0900

    Add notes model and connect database on startup

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+
 from app.api import ping
+from app.db import engine, database, metadata
+
+# create tables
+metadata.create_all(engine)
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(ping.router)
