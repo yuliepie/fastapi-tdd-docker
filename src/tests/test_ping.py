c054e643570cdba4518725c5bbb152e71d5f1357
Yulie Jung <yuliekorea@gmail.com>
2021-12-03 15:41:57 +0900

    Setup project, tests, Postgres with docker

---
+++ src/tests/test_ping.py
@@ -0,0 +1,9 @@
+from starlette.testclient import TestClient
+
+from app.main import app
+
+
+def test_ping(test_app):
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!"}
